d8531516e3ec8e459a31aa588ffe62554cbcbeae
jcd4stg <huynguyenlyn@gmail.com>
2024-02-16 04:44:40 +0700

    Handle auth session, add databse

---
+++ Messenger/Controllers/ProfileViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ProfileViewController.swift
+//  Messenger
+//
+//  Created by lynnguyen on 12/02/2024.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
